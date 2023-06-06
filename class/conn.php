1729bdde80b7deccccfcbd3a79942a64feecbe6f
Subbrat <m.subbrat@outlook.com>
2023-06-06 22:08:42 +0530

    Update conn.php

--- class/conn.php
+++ class/conn.php
@@ -1,7 +1,7 @@
 <?php
 $servername = "localhost";
 $username = "root";
-$password = "";
+$password = "Admin@1234";
 $dbname = "ldmon";
 
 $connection = mysqli_connect($servername, $username, $password, $dbname);
