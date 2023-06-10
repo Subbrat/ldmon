3088df808556328a35d36dec3eba9e76438bda33
Subbrat <m.subbrat@outlook.com>
2023-06-11 00:27:26 +0530

    additional top pending

--- sudo/pages/index.php
+++ sudo/pages/index.php
@@ -3,21 +3,26 @@ session_start();
 if (isset($_SESSION['verified'])) {
     // Session exists, display the HTML table contents
 ?>
-<!DOCTYPE html>
-<html>
-<?php include('./../../class/com.php'); ?>
+    <!DOCTYPE html>
+    <html>
+    <?php include('./../../class/com.php'); ?>
 
-<head>
-    <link rel="stylesheet" href="./../../class/css/s6css.css">
-</head>
+    <head>
+        <link rel="stylesheet" href="./../../class/css/s6css.css">
+        <style>
+            body {
+                padding-top: 10px;
+            }
+        </style>
+    </head>
 
-<body class="s-grey">
-    <?php include('./includes/header.php'); ?>
-    <?php include('./includes/sidebar.php'); ?>
-    <?php include('./tables.php'); ?>
-</body>
+    <body class="s-grey">
+        <?php include('./includes/header.php'); ?>
+        <?php include('./includes/sidebar.php'); ?>
+        <?php include('./tables.php'); ?>
+    </body>
 
-</html>
+    </html>
 <?php
 } else {
     // Session does not exist, navigate to index page
