9608bb38e9f3af4a082d9f2ea244c4c7946a0e34
Subbrat <m.subbrat@outlook.com>
2023-06-10 23:59:39 +0530

    cc

--- class/com.php
+++ class/com.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="./css/s6css.css">
-    <title>Super User Login • Ldmon</title>
+    <link rel="stylesheet" href="./../../class/css/s6css.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/5.0.0/normalize.min.css">
--- sudo/pages/index.php
+++ sudo/pages/index.php
@@ -3,17 +3,21 @@ session_start();
 if (isset($_SESSION['verified'])) {
     // Session exists, display the HTML table contents
 ?>
-    <!DOCTYPE html>
-    <html>
-    <?php include('./../../class/com.php'); ?>
+<!DOCTYPE html>
+<html>
+<?php include('./../../class/com.php'); ?>
 
-    <body class="s-grey">
-        <?php include('./includes/header.php'); ?>
-        <?php include('./includes/sidebar.php'); ?>
-        <?php include('./tables.php'); ?>
-    </body>
+<head>
+    <link rel="stylesheet" href="./../../class/css/s6css.css">
+</head>
 
-    </html>
+<body class="s-grey">
+    <?php include('./includes/header.php'); ?>
+    <?php include('./includes/sidebar.php'); ?>
+    <?php include('./tables.php'); ?>
+</body>
+
+</html>
 <?php
 } else {
     // Session does not exist, navigate to index page
