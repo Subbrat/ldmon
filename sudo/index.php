94b0227bf1958d290bcc53e73a3aff553726af00
Subbrat <m.subbrat@outlook.com>
2023-06-12 00:40:01 +0530

    footer -- a

--- sudo/index.php
+++ sudo/index.php
@@ -3,29 +3,29 @@ session_start();
 if (isset($_SESSION['verified'])) {
     // Session exists, display the HTML table contents
 ?>
-    <!DOCTYPE html>
-    <html>
-    <?php include('./../class/com.php'); ?>
+<!DOCTYPE html>
+<html>
+<?php include('./../class/com.php'); ?>
 
-    <head>
-        <link rel="stylesheet" href="./../class/css/s6css.css">
-        <link rel="stylesheet" href="./css/base.css">
-        <style>
-            body {
-                padding-top: 10px;
-            }
-        </style>
-    </head>
+<head>
+    <link rel="stylesheet" href="./../class/css/s6css.css">
+    <link rel="stylesheet" href="./css/base.css">
+    <style>
+    body {
+        padding-top: 10px;
+    }
+    </style>
 
-    <body class="s-grey">
-        <?php include('./includes/header.php'); ?>
-        <?php include('./includes/sidebar.php'); ?>
-        <div id="main" class="s-light-grey">
-            content goes fom here
-        </div>
-    </body>
+<body class="s-grey">
+    <?php include('./includes/header.php'); ?>
+    <?php include('./includes/sidebar.php'); ?>
+    <div id="main" class="s-light-grey">
+        content goes fom here
+    </div>
+    <?php include('./includes/footer.php'); ?>
+</body>
 
-    </html>
+</html>
 <?php
 } else {
     // Session does not exist, navigate to index page
