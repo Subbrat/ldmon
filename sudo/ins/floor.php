21363687fea1cdf47bb7da822f3dc21eb067a6b1
Subbrat <m.subbrat@outlook.com>
2023-06-14 22:29:16 +0530

    chks

--- sudo/ins/floor.php
+++ sudo/ins/floor.php
@@ -1,24 +1,39 @@
 <?php
-// session_start();
+//session_start();
 if (isset($_SESSION['verified'])) {
     include('./../class/conn.php');
+    $floorName = "";
+    $location = "";
+    $errorMessage = "";
+
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
         $floorName = $_POST["floorName"];
         $location = $_POST["location"];
+
         // Check if the floor name already exists
         $checkQuery = "SELECT * FROM floor WHERE floor_name = '$floorName'";
         $checkResult = $connection->query($checkQuery);
+
         if ($checkResult->num_rows > 0) {
-            echo "<p class='error-message'>Floor name already exists. Please enter a unique floor name.</p>";
+            $errorMessage = "Floor name already exists. Please enter a unique floor name.";
         } else {
             // Insert the new floor data into the table
             $insertQuery = "INSERT INTO floor (floor_name, location) VALUES ('$floorName', '$location')";
+
             if ($connection->query($insertQuery) === true) {
                 echo "<p class='success-message'>Floor data added successfully.</p>";
+                // Clear form fields after successful submission
+                $floorName = "";
+                $location = "";
             } else {
-                echo "<p class='error-message'>Error: " . $connection->error . "</p>";
+                $errorMessage = "Error: " . $connection->error;
             }
         }
+    } else {
+        // Reset variables when the page is loaded without form submission
+        $floorName = "";
+        $location = "";
+        $errorMessage = "";
     }
 }
 ?>
@@ -42,11 +57,20 @@ if (isset($_SESSION['verified'])) {
 <body>
     <form method="POST">
         <label for="floorName">Floor Name:</label>
-        <input type="text" name="floorName" required>
+        <input type="text" name="floorName" value="<?php echo $floorName; ?>" autocomplete="off" required>
         <label for="location">Location:</label>
-        <input type="text" name="location">
+        <input type="text" name="location" value="<?php echo $location; ?>" autocomplete="off">
         <button type="submit">Add Floor</button>
     </form>
+    <?php if (!empty($errorMessage)) : ?>
+    <p class="error-message"><?php echo $errorMessage; ?></p>
+    <?php endif; ?>
+    <script>
+    // Clear form fields when the page is refreshed
+    if (window.history.replaceState) {
+        window.history.replaceState(null, null, window.location.href);
+    }
+    </script>
 </body>
 
 </html>
