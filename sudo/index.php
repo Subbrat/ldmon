786ae95ddb88ce61afa5e7021847d5045b7269e4
Subbrat <m.subbrat@outlook.com>
2023-06-14 22:15:46 +0530

    Update index.php

--- sudo/index.php
+++ sudo/index.php
@@ -1,36 +1,40 @@
-<?php
-session_start();
-if (isset($_SESSION['verified'])) {
-    // Session exists, display the HTML table contents
-?>
-<!DOCTYPE html>
-<html>
-<?php include('./../class/com.php'); ?>
+    <?php
+    session_start();
+    if (isset($_SESSION['verified'])) {
+        // Session exists, display the HTML table contents
+    ?>
+    <!DOCTYPE html>
+    <html>
+    <?php include('./../class/com.php'); ?>
 
-<head>
-    <link rel="stylesheet" href="./../class/css/s6css.css">
-    <link rel="stylesheet" href="./css/base.css">
-    <style>
-    body {
-        padding-top: 10px;
+    <head>
+        <link rel="stylesheet" href="./../class/css/s6css.css">
+        <link rel="stylesheet" href="./css/base.css">
+        <style>
+        body {
+            padding-top: 10px;
+        }
+        </style>
+
+    <body class="s-grey">
+        <?php include('./includes/header.php');
+            include('./includes/sidebar.php'); ?>
+        <div id="main" class="s-light-grey">
+            <!--  -->
+            <?php include('./ins/floor.php'); ?>
+            <!--  -->
+            <? //php include('./ins/tables.php'); 
+                ?>
+        </div>
+        <?php include('./includes/footer.php'); ?>
+    </body>
+
+    </html>
+    <?php
+    } else {
+        $_SESSION = array();
+        session_destroy();
+        header("Location:./../login");
+        exit;
     }
-    </style>
-
-<body class="s-grey">
-    <?php include('./includes/header.php'); ?>
-    <?php include('./includes/sidebar.php'); ?>
-    <div id="main" class="s-light-grey">
-        <?php include('./ins/tables.php'); ?>
-    </div>
-    <?php include('./includes/footer.php'); ?>
-</body>
-
-</html>
-<?php
-} else {
-    $_SESSION = array();
-    session_destroy();
-    header("Location:./../login");
-    exit;
-}
-?>
+    ?>
