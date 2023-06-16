520e80c5ee5687756cd952f2d102de8bf5df6be1
Subbrat <m.subbrat@outlook.com>
2023-06-16 18:15:54 +0530

    Update index.php

--- sudo/index.php
+++ sudo/index.php
@@ -3,34 +3,30 @@
     if (isset($_SESSION['verified'])) {
         // Session exists, display the HTML table contents
     ?>
-    <!DOCTYPE html>
-    <html>
-    <?php include('./../class/com.php'); ?>
+        <!DOCTYPE html>
+        <html>
+        <?php include('./../class/com.php'); ?>
 
-    <head>
-        <link rel="stylesheet" href="./../class/css/s6css.css">
-        <link rel="stylesheet" href="./css/base.css">
-        <style>
-        body {
-            padding-top: 10px;
-        }
-        </style>
+        <head>
+            <link rel="stylesheet" href="./../class/css/s6css.css">
+            <link rel="stylesheet" href="./css/base.css">
+            <style>
+                body {
+                    padding-top: 10px;
+                }
+            </style>
 
-    <body class="s-grey">
-        <?php include('./includes/header.php');
+        <body class="s-grey">
+            <?php include('./includes/header.php');
             include('./includes/sidebar.php');
             ?>
-        <div id="main" class="s-lgrey">
-            <div style="height:70px;" class=""></div>
-            <!--  -->
-            <?php include('./ins/readtable.php');
-                ?>
-            <!--  -->
-        </div>
-        <?php include('./includes/footer.php'); ?>
-    </body>
+            <div id="main" class="s-lightgrey">
+                <div style="height:70px;" class=""></div>
+            </div>
+            <?php include('./includes/footer.php'); ?>
+        </body>
 
-    </html>
+        </html>
     <?php
     } else {
         $_SESSION = array();
