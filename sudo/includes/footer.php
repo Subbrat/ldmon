f778f6d5aee5a6331385e53ce391638bc191030a
Subbrat <m.subbrat@outlook.com>
2023-06-16 18:53:22 +0530

    Update footer.php

--- sudo/includes/footer.php
+++ sudo/includes/footer.php
@@ -3,18 +3,19 @@
     <link rel="stylesheet" href="./../css/base.css">
     <style>
     #footbar {
-        background-color: rgba(22, 22, 23, 0.8);
+        background-color: #000;
         color: white;
         padding: 0px 13px;
         position: fixed;
         left: 0;
         bottom: 0;
         width: 100%;
+        height: 35px;
     }
     </style>
 </head>
 <div id="main">
     <footer class="" id="footbar" style="z-index:10">
-        <h4>LDMON v1</h4>
+        <h6>LDMON v1</h6>
     </footer>
 </div>
