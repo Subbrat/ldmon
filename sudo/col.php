a896fd6b39a8c72bba113e7a1a05b538c0df9360
Subbrat <m.subbrat@outlook.com>
2023-06-16 19:45:06 +0530

    Update col.php

--- sudo/col.php
+++ sudo/col.php
@@ -6,46 +6,46 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Color Palette Template</title>
     <style>
-        /* CSS styles using the color palette */
-        .color-card {
-            display: flex;
-            flex-direction: column;
-            align-items: center;
-            justify-content: center;
-            width: 200px;
-            height: 200px;
-            margin: 10px;
-            text-align: center;
-            font-weight: bold;
-            font-family: Arial, sans-serif;
-            border-radius: 5px;
-        }
+    /* CSS styles using the color palette */
+    .color-card {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+        width: 200px;
+        height: 200px;
+        margin: 10px;
+        text-align: center;
+        font-weight: bold;
+        font-family: Arial, sans-serif;
+        border-radius: 5px;
+    }
 
-        .dark-blue {
-            background-color: #1D3557;
-            color: #F1FAEE;
-        }
+    .dark-blue {
+        background-color: #1D3557;
+        color: #F1FAEE;
+    }
 
-        .off-white {
-            background-color: #F1FAEE;
-            color: #1D3557;
-        }
+    .off-white {
+        background-color: #F1FAEE;
+        color: #1D3557;
+    }
 
-        .light-blue {
-            background-color: #A8DADC;
-        }
+    .light-blue {
+        background-color: #A8DADC;
+    }
 
-        /* Additional styling for demonstration */
-        body {
-            margin: 0;
-            padding: 0;
-            display: flex;
-            flex-wrap: wrap;
-            justify-content: center;
-            align-items: center;
-            min-height: 100vh;
-            background-color: #F1FAEE;
-        }
+    /* Additional styling for demonstration */
+    body {
+        margin: 0;
+        padding: 0;
+        display: flex;
+        flex-wrap: wrap;
+        justify-content: center;
+        align-items: center;
+        min-height: 100vh;
+        background-color: #F1FAEE;
+    }
     </style>
 </head>
 
