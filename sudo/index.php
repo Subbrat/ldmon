c4a5f588dba229c94a8c7a36f9c240c08815ed6f
Subbrat <m.subbrat@outlook.com>
2023-06-17 11:28:41 +0530

    Update index.php

--- sudo/index.php
+++ sudo/index.php
@@ -3,37 +3,37 @@
     if (isset($_SESSION['verified'])) {
         // Session exists, display the HTML table contents
     ?>
-    <!DOCTYPE html>
-    <html>
-    <?php include('./../class/com.php'); ?>
+        <!DOCTYPE html>
+        <html>
+        <?php include('./../class/com.php'); ?>
 
-    <head>
-        <link rel="stylesheet" href="./../class/css/s6css.css">
-        <link rel="stylesheet" href="./css/base.css">
-        <style>
-        body {
-            padding-top: 10px;
-        }
-        </style>
+        <head>
+            <link rel="stylesheet" href="./../class/css/s6css.css">
+            <link rel="stylesheet" href="./css/base.css">
+            <style>
+                body {
+                    padding-top: 10px;
+                }
+            </style>
 
-    <body class="s-grey">
-        <?php include('./includes/header.php');
+        <body class="s-grey">
+            <?php include('./includes/header.php');
             include('./includes/sidebar.php'); ?>
-        <div id="main" class="s-light-grey">
-            <!--  -->
-            <div style="height:70px;" class=""></div>
-            </ /?php include('./ins/cw.php'); ?>
-            <? //php include('./ins/cc.php');
+            <div id="main" class="s-light-grey">
+                <!--  -->
+                <div style="height:70px;" class=""></div>
+                </ /?php include('./ins/cw.php'); ?>
+                <? //php include('./ins/cc.php');
                 ?>
-            <!--  -->
-            <? //php include('./ins/floor.php');
+                <!--  -->
+                <?php include('./ins/floor.php');
                 // include('./ins/cc.php');
                 ?>
-        </div>
-        <?php include('./includes/footer.php'); ?>
-    </body>
+            </div>
+            <?php include('./includes/footer.php'); ?>
+        </body>
 
-    </html>
+        </html>
     <?php
     } else {
         $_SESSION = array();
