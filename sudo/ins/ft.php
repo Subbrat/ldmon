f0439a8ec50221174463c74c689c98a52a1dce32
Subbrat <m.subbrat@outlook.com>
2023-06-17 12:22:02 +0530

    Update ft.php

--- sudo/ins/ft.php
+++ sudo/ins/ft.php
@@ -1,5 +1,6 @@
 <head>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.3.0/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/subbrat/el@main/css/s6css.css" />
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/subbrat/el@main/css/ga.css" />
     <style>
     input[type=text] {
         width: 350px;
@@ -35,39 +36,40 @@
         width: 69px;
         font-size: x-large;
     }
+
+    label {
+        color: #000;
+    }
     </style>
 </head>
-
-<body>
-    <div class="s-border s-red" id="">
-        <div class="card">
-            <div class="card-body">
-                <form method="POST" action="./ins/fts.php">
-                    <!-- Update the action attribute -->
-                    <label class=" catlabel">floor</label><br>
-                    <label for="floorName">Floor Name:</label>
-                    <input type="text" name="floorName" autocomplete="off" required>
-                    <label for="location">Location:</label>
-                    <input type="text" name="location" autocomplete="off">
-                    <button type="submit" class="s-btn s-round-large button">Add Floor</button>
-                </form>
-                <?php if (!empty($errorMessage)) : ?>
-                <div class="alert alert-danger mt-3" id="errorMessage"><?php echo $errorMessage; ?></div>
-                <script>
-                setTimeout(function() {
-                    document.getElementById('errorMessage').remove();
-                }, 3000);
-                </script>
-                <?php endif; ?>
-                <?php if (!empty($successMessage)) : ?>
-                <div class="alert alert-success mt-3" id="successMessage"><?php echo $successMessage; ?></div>
-                <script>
-                setTimeout(function() {
-                    document.getElementById('successMessage').remove();
-                }, 3000);
-                </script>
-                <?php endif; ?>
-            </div>
+<div class="s-border s-red" id="">
+    <div class="card">
+        <div class="card-body">
+            <form method="POST" action="./ins/fts.php">
+                <!-- Update the action attribute -->
+                <label class=" catlabel">floor</label><br>
+                <label for="floorName s-green">Floor Name:</label>
+                <input type="text" name="floorName" autocomplete="off" required>
+                <label for="location">Location:</label>
+                <input type="text" name="location" autocomplete="off">
+                <button type="submit" class="s-btn s-round-large button">Add Floor</button>
+            </form>
+            <?php if (!empty($errorMessage)) : ?>
+            <div class="alert alert-danger mt-3" id="errorMessage"><?php echo $errorMessage; ?></div>
+            <script>
+            setTimeout(function() {
+                document.getElementById('errorMessage').remove();
+            }, 3000);
+            </script>
+            <?php endif; ?>
+            <?php if (!empty($successMessage)) : ?>
+            <div class="alert alert-success mt-3" id="successMessage"><?php echo $successMessage; ?></div>
+            <script>
+            setTimeout(function() {
+                document.getElementById('successMessage').remove();
+            }, 3000);
+            </script>
+            <?php endif; ?>
         </div>
     </div>
-</body>
+</div>
