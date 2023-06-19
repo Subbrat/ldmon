a297aea269fdb8f366ab5ef4bb2442f70b95868c
Subbrat <m.subbrat@outlook.com>
2023-06-19 19:57:43 +0530

    Update ft.php

--- sudo/ins/ft.php
+++ sudo/ins/ft.php
@@ -2,69 +2,68 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/subbrat/el@main/css/s6css.css" />
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/subbrat/el@main/css/ga.css" />
     <style>
-        input[type=text] {
-            width: 350px;
-            padding: 4px 7px;
-            margin: 8px 0;
-            box-sizing: border-box;
-            border: none;
-            border-bottom: 1px solid red;
-            background-color: grey;
-        }
+    input[type=text] {
+        width: 350px;
+        padding: 4px 7px;
+        margin: 8px 0;
+        box-sizing: border-box;
+        border: none;
+        border-bottom: 1px solid red;
+        background-color: grey;
+    }
 
-        input[type=text]:focus {
-            border: 1px solid #fff;
-            outline: 1px solid green;
-            background-color: lightgrey;
-        }
+    input[type=text]:focus {
+        border: 1px solid #fff;
+        outline: 1px solid green;
+        background-color: lightgrey;
+    }
 
-        .button {
-            background-color: #4CAF50;
-            border: none;
-            color: white;
-            padding: 4px 7px;
-            text-align: center;
-            text-decoration: none;
-            display: inline-block;
-            font-size: 16px;
-            margin: auto;
-            cursor: pointer;
-        }
+    .button {
+        background-color: #4CAF50;
+        border: none;
+        color: white;
+        padding: 4px 7px;
+        text-align: center;
+        text-decoration: none;
+        display: inline-block;
+        font-size: 16px;
+        margin: auto;
+        cursor: pointer;
+    }
 
-        .catlabel {
-            border-bottom: 2px solid green;
-            width: 69px;
-            font-size: x-large;
-        }
+    .catlabel {
+        border-bottom: 2px solid green;
+        width: 69px;
+        font-size: x-large;
+    }
 
-        label {
-            color: #000;
-        }
+    label {
+        color: #000;
+    }
 
-        /* Added styles for error and success messages */
-        .alert {
-            display: none;
-            margin-top: 10px;
-        }
+    /* Added styles for error and success messages */
+    .alert {
+        display: none;
+        margin-top: 10px;
+    }
 
-        .alert-danger {
-            color: red;
-        }
+    .alert-danger {
+        color: red;
+    }
 
-        .alert-success {
-            color: green;
-        }
+    .alert-success {
+        color: green;
+    }
 
-        #floorf {
-            height: 200px;
-        }
+    #floorf {
+        height: 200px;
+    }
     </style>
 </head>
 <div class="s-border s-red">
     <div class="card" id="floorf">
         <div class="card-body">
             <form id="addFloorForm" method="POST" action="./ins/fts.php">
-                <!-- Update the action attribute -->
                 <label class=" catlabel">floor</label><br>
                 <label for="floorName s-green">Floor Name:</label>
                 <input type="text" name="floorName" autocomplete="off" required>
