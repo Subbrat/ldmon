4f821020110cc591b7e62bf3a133f5a6683fa1b3
Subbrat <m.subbrat@outlook.com>
2023-06-19 11:30:56 +0530

    Update conn.php

--- class/conn.php
+++ class/conn.php
@@ -1,11 +1,11 @@
-<?php
-$servername = "localhost";
-$username = "root";
-$password = "cetadelta";
-$dbname = "ldmon";
+        <?php
+        $servername = "localhost";
+        $username = "root";
+        $password = "cetadelta";
+        $dbname = "ldmon";
 
-$connection = mysqli_connect($servername, $username, $password, $dbname);
+        $connection = mysqli_connect($servername, $username, $password, $dbname);
 
-if (!$connection) {
-    die("Connection failed: " . mysqli_connect_error());
-}
+        if (!$connection) {
+            die("Connection failed: " . mysqli_connect_error());
+        }
