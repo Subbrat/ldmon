4b5c25da234e8ec48480f501fcd5361fdf47a3f5
Subbrat <m.subbrat@outlook.com>
2023-06-21 04:23:45 +0530

    Revert "Update index.php"
    This reverts commit 58b5165cc83960c306883d2e84a604f8eabf19f7.

--- sudo/index.php
+++ sudo/index.php
@@ -2,82 +2,74 @@
 session_start();
 if (isset($_SESSION['verified'])) {
 ?>
-    <!DOCTYPE html>
-    <html>
-    <?php include('./../class/com.php'); ?>
+<!DOCTYPE html>
+<html>
+<?php include('./../class/com.php'); ?>
 
-    <head>
+<head>
 
-        <link rel="stylesheet" href="./../class/css/s6css.css">
-        <link rel="stylesheet" href="./class/css/s6css.css">
-        <link rel="stylesheet" href="./css/base.css">
-        <style>
-            body {
-                padding-top: 10px;
-                padding-bottom: 70px;
-            }
-        </style>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
-    </head>
+    <link rel="stylesheet" href="./../class/css/s6css.css">
+    <link rel="stylesheet" href="./class/css/s6css.css">
+    <link rel="stylesheet" href="./css/base.css">
+    <style>
+    body {
+        padding-top: 10px;
+        padding-bottom: 70px;
+    }
+    </style>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+</head>
 
-    <body class="s-grey">
-        <?php include('./includes/header.php');
+<body class="s-grey">
+    <?php include('./includes/header.php');
         include('./includes/sidebar.php'); ?>
-<<<<<<< Updated upstream
     <div id="main" class="s-light-grey">
         <!--  -->
         <!-- <div style="height:70px;" clasxls=""></div> -->
         <!--  -->
         <?php include('./ins/ft.php');
-=======
-        <div id="main" class="s-light-grey">
-            <!--  -->
-            <!-- <div style="height:70px;" clasxls=""></div> -->
-            <!--  -->
-            <?php include('./ins/ft.php');
->>>>>>> Stashed changes
             ?>
-            <? //php include('./ins/readonlytable.php');
+        <? //php include('./ins/readonlytable.php');
             ?>
-        </div>
-        <?php include('./includes/footer.php'); ?>
-        <script>
-            $(document).ready(function() {
-                $('form').submit(function(e) {
-                    e.preventDefault();
-                    var form = $(this);
-                    var url = form.attr('action');
-                    var formData = form.serialize();
-                    $.ajax({
-                        type: 'POST',
-                        url: url,
-                        data: formData,
-                        dataType: 'json',
-                        success: function(response) {
-                            if (response.errorMessage) {
-                                $('#errorMessage').text(response.errorMessage);
-                                $('#errorMessage').show();
-                                setTimeout(function() {
-                                    $('#errorMessage').fadeOut();
-                                }, 2500);
-                            } else {
-                                $('#successMessage').text(response.successMessage);
-                                $('#successMessage').show();
-                                setTimeout(function() {
-                                    $('#successMessage').fadeOut();
-                                }, 2500);
-                            }
-                        },
-                        error: function(xhr, status, error) {
-                            console.log(xhr.responseText);
-                        }
-                    });
-                });
+    </div>
+    <?php include('./includes/footer.php'); ?>
+    <script>
+    $(document).ready(function() {
+        $('form').submit(function(e) {
+            e.preventDefault();
+            var form = $(this);
+            var url = form.attr('action');
+            var formData = form.serialize();
+            $.ajax({
+                type: 'POST',
+                url: url,
+                data: formData,
+                dataType: 'json',
+                success: function(response) {
+                    if (response.errorMessage) {
+                        $('#errorMessage').text(response.errorMessage);
+                        $('#errorMessage').show();
+                        setTimeout(function() {
+                            $('#errorMessage').fadeOut();
+                        }, 2500);
+                    } else {
+                        $('#successMessage').text(response.successMessage);
+                        $('#successMessage').show();
+                        setTimeout(function() {
+                            $('#successMessage').fadeOut();
+                        }, 2500);
+                    }
+                },
+                error: function(xhr, status, error) {
+                    console.log(xhr.responseText);
+                }
             });
-        </script>
-    </body>
+        });
+    });
+    </script>
+</body>
 
-    </html>
+</html>
 <?php
 } else {
     $_SESSION = array();
